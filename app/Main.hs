bfd8115f73285f90d98d8c3d4316904488a3fe81
Niels GEORGE-PEILLEX <niels.george-peillex@epitech.eu>
2019-03-27 16:07:16 +0100

    Better args parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,44 +1,43 @@
 module Main where
 
 import System.Environment
-import System.Exit
 import Numeric.Natural
+import System.Exit
+import Data.Maybe
 import Data.List
 import Data.Char
+import Text.Read
 
 main :: IO ()
-main = getArgs >>= parse
+main = getArgs >>= parse >>= putStrLn
 
 -- PARSING ARGS -------------------------------------
 
-parse [n, e, path] = do
-    n <- checkNumber n
-    e <- checkNumber e
-    putStrLn "WORKS !!"
-
-parse otherwise = do
-    usage
-    exitError
+parse :: [String] -> IO (String)
+parse [] = usage >> exitError
+parse [n, e] = usage >> exitError
+parse [n, e, path]
+    | isNothing checkN = usage >> exitError
+    | isNothing checkE = usage >> exitError
+    | otherwise = getFile $ path
+    where
+        checkN = readMaybe n :: Maybe Int
+        checkE =  readMaybe e :: Maybe Double
+parse otherwise = usage >> exitError
 
 -- READ FILE ----------------------------------------
 
-
+getFile :: String -> IO (String)
+getFile path = readFile path
 
 -- TOOLS --------------------------------------------
 
-checkNumber :: String -> IO (Int)
-checkNumber n = do
-    if checkIsNumber n == True
-        then return (read n::Int)
-        else usage >> exitError
-
-checkIsNumber :: String -> Bool
-checkIsNumber n = case (reads n) :: [(Natural, String)] of
-    [(_, "")] -> True
-    _         -> False
-
 usage :: IO ()
-usage = putStrLn "USAGE: ./imageCompressor n e IN\n\n\tn\t\tnumber of colors in the final image\n\te\t\tconvergence limit\n\tIN\tpath to the file containing the colors of the pixels"
+usage = putStrLn "USAGE: ./imageCompressor n e IN\n\n\tn\tnumber of colors in the final image\n\te\tconvergence limit\n\tIN\tpath to the file containing the colors of the pixels"
+
+exitError :: IO a
 exitError = exitWith (ExitFailure 84)
+
+exit :: IO Int
 exit = exitWith ExitSuccess
 
